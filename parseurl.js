f49c9b003d198e9d3439afb0c314dc3b82e2cba9
joogps <joaogabrielps13@hotmail.com>
2019-03-14 19:43:57 -0300

    Update parseurl.js

--- parseurl.js
+++ parseurl.js
@@ -1,11 +1,11 @@
 function youtube_video(url){
 	var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
 	var match = url.match(regExp);
-	return (match&&match[7].length==11)? match[7] : false;
+	return (match && match[7].length==11)? match[7] : false;
 }
 
 function youtube_playlist(url){
 	var regExp = /^.*((youtu.be\/)|list=([^#\&\?]+))/;
 	var match = url.match(regExp);
-	return (match&&match[3].length==34)? match[3] : false;
+	return (match && match[3].length==34)? match[3] : false;
 }
